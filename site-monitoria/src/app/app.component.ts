bb803465f40693673ee15f893b2421082cb32ba6
Lucas Zoser Nunes Costa <lucaszoser@dinfor.uniceub>
2023-08-22 18:49:23 -0300

    primeira parte nav-bar

---
+++ site-monitoria/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'homepage',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'site-monitoria';
+}
